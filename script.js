ddf6ea06fd382cab5de4bce07cfcbad405f0a1c0
Franco Palacios <francopalacios7@yahoo.com.ar>
2023-04-27 23:45:37 -0300

    1a10

---
+++ script.js
@@ -0,0 +1,16 @@
+let miNombre = "Franco" 
+let miApellido= "Palacios"
+let miEdad= 19
+let miMascota= "Rocco"
+let edadMacota= 6
+
+let nombreCompleto= miNombre + " " + miApellido
+
+let textoPresentacion= "Soy" + " " + miNombre + " " + miApellido +","+ "tengo" + " " + miEdad + " " + "años" + ", y mi perro se llama" + " " + miMascota +  " " + "y tiene" + " " + edadMacota + " " + "años."
+
+let sumaEdades = miEdad + edadMacota
+let restaEdades = miEdad - edadMacota
+let productoEdades = miEdad * edadMacota
+let divisionEdades = miEdad / edadMacota
+
+
